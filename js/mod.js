db36c855c712d3888d2cdf622ad0890919742562
georgegabrdev <georgegabr2014@gmail.com>
2026-06-10 16:38:47 -0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -5,7 +5,7 @@ let modInfo = {
 	pointsName: "points",
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new ExpantaNum (10), // Used for hard resets and new players
+	initialStartPoints: new MetaNum (10), // Used for hard resets and new players
 	
 	offlineLimit: 1,  // In hours
 }
