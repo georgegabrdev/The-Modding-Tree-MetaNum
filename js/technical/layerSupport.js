047ce685e58330c189fa0e6e9139c920f85e6b18
georgegabrdev <georgegabr2014@gmail.com>
2026-06-10 16:39:44 -0300

    Replace ExpantaNum with MetaNum in layerSupport.js

--- js/technical/layerSupport.js
+++ js/technical/layerSupport.js
@@ -1,11 +1,11 @@
 var layers = {}
-const EN = ExpantaNum
-const ExpantaNumZero = new ExpantaNum(0)
-const ExpantaNumOne = new ExpantaNum(1)
-const ExpantaNumNaN = new ExpantaNum(NaN)
-const decimalZero = ExpantaNumZero
-const decimalOne= ExpantaNumOne
-const decimalNaN = ExpantaNumNaN
+const EN = MetaNum
+const MetaNumZero = new MetaNum(0)
+const MetaNumOne = new MetaNum(1)
+const MetaNumNaN = new MetaNum(NaN)
+const decimalZero = MetaNumZero
+const decimalOne= MetaNumOne
+const decimalNaN = MetaNumNaN
 function layerShown(layer){
     return tmp[layer].layerShown;
 }
@@ -123,7 +123,7 @@ function setupLayer(layer){
                 if (layers[layer].buyables[thing].unlocked === undefined)
                     layers[layer].buyables[thing].unlocked = true
                 layers[layer].buyables[thing].canBuy = function() {return canBuyBuyable(this.layer, this.id)}
-                if (layers[layer].buyables[thing].purchaseLimit === undefined) layers[layer].buyables[thing].purchaseLimit = new ExpantaNum(Infinity)
+                if (layers[layer].buyables[thing].purchaseLimit === undefined) layers[layer].buyables[thing].purchaseLimit = new MetaNum(Infinity)
         
             }  
     
@@ -179,13 +179,13 @@ function setupLayer(layer){
     if(!layers[layer].componentStyles) layers[layer].componentStyles = {}
     if(layers[layer].symbol === undefined) layers[layer].symbol = layer.charAt(0).toUpperCase() + layer.slice(1)
     if(layers[layer].unlockOrder === undefined) layers[layer].unlockOrder = []
-    if(layers[layer].gainMult === undefined) layers[layer].gainMult = ExpantaNumOne
-    if(layers[layer].gainExp === undefined) layers[layer].gainExp = ExpantaNumOne
-    if(layers[layer].directMult === undefined) layers[layer].directMult = ExpantaNumOne
+    if(layers[layer].gainMult === undefined) layers[layer].gainMult = MetaNumOne
+    if(layers[layer].gainExp === undefined) layers[layer].gainExp = MetaNumOne
+    if(layers[layer].directMult === undefined) layers[layer].directMult = MetaNumOne
     if(layers[layer].type === undefined) layers[layer].type = "none"
     if(layers[layer].base === undefined || layers[layer].base <= 1) layers[layer].base = 2
-    if(layers[layer].softcap === undefined) layers[layer].softcap = new ExpantaNum("e1e7")
-    if(layers[layer].softcapPower === undefined) layers[layer].softcapPower = new ExpantaNum("0.5")
+    if(layers[layer].softcap === undefined) layers[layer].softcap = new MetaNum("e1e7")
+    if(layers[layer].softcapPower === undefined) layers[layer].softcapPower = new MetaNum("0.5")
     if(layers[layer].displayRow === undefined) layers[layer].displayRow = layers[layer].row
     if(layers[layer].name === undefined) layers[layer].name = layer
     if(layers[layer].layerShown === undefined) layers[layer].layerShown = true
