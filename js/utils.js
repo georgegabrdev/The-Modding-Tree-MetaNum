2d0c68b1442648f80838ec7d6a57e1b0add9a4ec
georgegabrdev <georgegabr2014@gmail.com>
2026-06-11 14:14:20 -0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -182,7 +182,7 @@ function goBack(layer) {
 
 function layOver(obj1, obj2) {
 	for (let x in obj2) {
-		if (obj2[x] instanceof ExpantaNum) obj1[x] = new ExpantaNum(obj2[x])
+		if (obj2[x] instanceof MetaNum) obj1[x] = new MetaNum(obj2[x])
 		else if (obj2[x] instanceof Object) layOver(obj1[x], obj2[x]);
 		else obj1[x] = obj2[x];
 	}
@@ -343,9 +343,9 @@ document.title = modInfo.name
 
 // Converts a string value to whatever it's supposed to be
 function toValue(value, oldValue) {
-	if (oldValue instanceof ExpantaNum) {
-		value = new ExpantaNum (value)
-		if (value.eq(ExpantaNumNaN)) return ExpantaNumZero
+	if (oldValue instanceof MetaNum) {
+		value = new MetaNum (value)
+		if (value.eq(MetaNumNaN)) return MetaNumZero
 		return value
 	}
 	if (!isNaN(oldValue)) 
