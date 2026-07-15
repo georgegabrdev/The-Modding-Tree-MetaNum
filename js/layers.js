2ac038fa08286d0ce2d4e2a2d4ceeb8974cbd2fa
georgegabrdev <georgegabr2014@gmail.com>
2026-07-15 15:56:47 -0300

    Reduce cost of Triple Points upgrade to 25

--- js/layers.js
+++ js/layers.js
@@ -16,7 +16,7 @@ addLayer("p", {
 		11: {
 	        title: "Triple Points",
 	        description: "Triple your point gain.",
-	        cost: new MetaNum(100),
+	        cost: new MetaNum(25),
 	
 	        effect() {
 	            return new MetaNum(3)
